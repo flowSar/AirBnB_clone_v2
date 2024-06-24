d5e7d3d612e06f4c7ac80182d8cdc42d2aea0bb1
flowsar <brahim.sar151@gmail.com>
2024-06-24 15:01:31 +0100

    script fro creating databases and user and grat priviliges to these databases

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,10 @@
+-- create two databases and and user=hbnb_dev
+-- grant ll privileges to hbnb_dev_db and select for performance_schema
+
+CREATE database IF NOT EXISTS hbnb_dev_db;
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'loclhost';
+FLUSH PRIVILEGES;
+CREATE database IF NOT EXISTS performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'loclhost';
+FLUSH PRIVILEGES;
