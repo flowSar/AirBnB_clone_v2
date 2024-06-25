e7b6b1d639457eab9ab5b157854848c7661167db
flowsar <brahim.sar151@gmail.com>
2024-06-25 22:02:55 +0100

    what's wrong

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(String(128), nullable=True)
