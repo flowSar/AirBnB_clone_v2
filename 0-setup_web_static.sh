2b3935b9f2071115017d21abd4740a6f997314aa
flowsar <brahim.sar151@gmail.com>
2024-07-11 18:57:42 +0100

    style

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-# install nginx
-sudo apt update
-sudo apt install nginx
 # create folder and index.html
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
 
 # inject simple html content in index.html
-tee /data/web_static/releases/test/index.html > /dev/null <<EOF
+sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
 <html>
   <head>
   </head>
@@ -20,20 +17,20 @@ EOF
 
 # check if symbol exsite and delete it 
 if [ -L /data/web_static/current ]; then
-    rm /data/web_static/current
+    sudo rm /data/web_static/current
 fi
 # creat symbolic link
-ln -s /data/web_static/releases/test /data/web_static/current
+sudo ln -s /data/web_static/releases/test /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group and everything inside
 sudo chown -R ubuntu:ubuntu /data/
 
 CONFIG_FILE="/etc/nginx/sites-available/default"
-
+uri="$uri"
 sudo cp $CONFIG_FILE ${CONFIG_FILE}.bak
 # Add the location block to the Nginx configuration file if it does not exist
 if ! grep -q "location /hbnb_static" $CONFIG_FILE; then
-    sudo sed -i "56i \\\tlocation /hbnb_static {\n \\t \\talias /data/web_static/current/;\n \\t \\ttry_files $uri /index.html =404; \n\\t}" $CONFIG_FILE
+    sudo sed -i "56i \\\tlocation /hbnb_static {\n \\t \\talias /data/web_static/current/;\n \\t \\ttry_files ${uri} /index.html =404; \n\\t}" $CONFIG_FILE
 
 fi
 
