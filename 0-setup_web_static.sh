f291dd120ad4db4bd1a9a385790c2e0eccaeea5e
flowsar <brahim.sar151@gmail.com>
2024-07-12 17:07:18 +0100

    copy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
-# doesn some I'm tired from it
-HOSTNAME=$(hostname)
+# Sets up web servers for static deployment
+# shellcheck disable=SC1004
 
-NGINX_INSTALLED=$(nginx -v 2>&1)
-if ! echo "$NGINX_INSTALLED" | grep -q "nginx"; then
-    sudo apt-get update > /dev/null
-    sudo apt-get -y install nginx > /dev/null
+# Setup root privilege
+if [ "$(id -u)" -ne 0 ]; then
+	echo "This script must be run as root."
+	exit 0
 fi
-# create folder and index.html
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
 
-# inject simple html content in index.html
-sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
+# Install nginx if not installed
+if ! dpkg -l nginx > "/dev/null"; then
+	echo "Nginx not installed, installing..."
+	apt update && apt -y upgrade
+	apt -y install nginx
+fi
+
+# Create required directories
+echo "Creating required directories and files"
+mkdir -p "/data/web_static/releases/test/"
+mkdir -p "/data/web_static/shared/"
+
+# Create index.html file with test content
+cat <<EOL > /data/web_static/releases/test/index.html
 <html>
   <head>
   </head>
@@ -21,44 +29,29 @@ sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
     Holberton School
   </body>
 </html>
-EOF
+EOL
 
-# check if symbol exsite and delete it 
-if [ -L /data/web_static/current ]; then
-    sudo rm /data/web_static/current
+# Create symlink to /data/web_static/releases/test/ directory
+ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+echo "Symlink created: /data/web_static/current -> /data/web_static/releases/test/"
+
+# Change ownership of data directory and files
+chown -hR ubuntu:ubuntu /data/
+
+# Updating nginx configuration to serve specified directory
+echo "Editing config file"
+if ! grep -q "^ *location /hbnb_static {" /etc/nginx/sites-available/default; then
+	sed -i '/server_name _;/a\
+\
+	location /hbnb_static {\
+		alias /data/web_static/current/;\
+		autoindex off;\
+	}' /etc/nginx/sites-available/default
 fi
-# creat symbolic link
-sudo ln -s /data/web_static/releases/test /data/web_static/current
 
-# Give ownership of the /data/ folder to the ubuntu user AND group and everything inside
-sudo chown -R ubuntu:ubuntu /data/
-
-CONFIG_FILE="/etc/nginx/sites-available/default"
-sudo cp $CONFIG_FILE ${CONFIG_FILE}.bak
-# Add the location block to the Nginx configuration file if it does not exist
-
-uri="\$uri"
-sudo tee $CONFIG_FILE > /dev/null <<EOF
-server {
- 
-        add_header X-Served-By $HOSTNAME;
-
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-                try_files $uri $uri/ =404;
-        }
-
-        location /hbnb_static {
-                alias /data/web_static/current/;
-                try_files  /index.html =404; 
-        }
-}
-EOF
-sudo service nginx restart
+# Restarting nginx service
+if nginx -t; then
+	service nginx restart
+else
+	echo "Please re-check configuration file"
+fi
