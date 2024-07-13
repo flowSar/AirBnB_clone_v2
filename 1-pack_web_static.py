9e44e1da708c408116c1d2b9853bdc8db4639158
flowsar <brahim.sar151@gmail.com>
2024-07-13 12:43:37 +0100

    use fabric lates version

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" pack web static"""
-from fabric.api import local
+"""pack web static"""
+
+from fabric import Connection
 from datetime import datetime
 
 
-def do_pack():
-    """ generates a .tgz archive from the contents of the web_static """
-    local("mkdir -p versions")
-
-    date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    arc_name = "versions/web_static_{}.tgz".format(date_time)
-
-    command = "tar -cvzf {} web_static".format(arc_name)
-    result = local(command)
+def do_deploy(archive_path):
+    """generates a .tgz archive from the contents of the web_static"""
+    local_conn = Connection('localhost')
+    local_conn.local('mkdir versions')
+    today = datetime.today().strftime('%Y%m%d%H%M%S')
+    arc_name = f"versions/web_static_{today}.tgz"
+    command = f"tar -cvzf {arc_name} test_folder/"
+    result = local_conn.local(command)
     if result.failed:
         return None
     return arc_name
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-"""module"""
+"""pack web static"""
 
-from fabric.api import local
+from fabric import Connection
 from datetime import datetime
 
 def do_deploy(archive_path):
-    """deply"""
-    pass
+    """generates a .tgz archive from the contents of the web_static"""
+    local_conn = Connection('localhost')
+    local_conn.local('mkdir versions')
+    today = datetime.today().strftime('%Y%m%d%H%M%S')
+    arc_name = f"versions/web_static_{today}.tgz"
+    command = f"tar -cvzf {arc_name} test_folder/"
+    result = local_conn.local(command)
+    if result.failed:
+        return None
+    return arc_name
+
+do_deploy('sar')
