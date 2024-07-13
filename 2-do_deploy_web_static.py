5ec76fdf64a90a9294c7384948e5f636b0cb55b0
flowsar <brahim.sar151@gmail.com>
2024-07-13 17:40:13 +0100

    root previ

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from os.path import exists
 
 env.hosts = ['35.175.64.12', '18.234.192.156']
 env.user = 'ubuntu'
-env.key_filename = ['~/.ssh/id_rsa']
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
@@ -19,14 +19,14 @@ def do_deploy(archive_path):
     try:
         put(archive_path, '/tmp/')
         # cd('/tmp/')
-        run(f'mkdir -p /data/web_static/releases/')
+        run(f'sudo mkdir -p /data/web_static/releases/')
         destination = f"/data/web_static/releases/{file_name}"
-        run(f"tar -xvzf /tmp/{file_name}.tgz -C {destination}")
-        run(f'rm -r /tmp/{file_name}.tgz')
+        run(f"sudo tar -xvzf /tmp/{file_name}.tgz -C {destination}")
+        run(f'sudo rm -r /tmp/{file_name}.tgz')
         # cd('/data/web_static/')
-        run('rm /data/web_static/current')
+        run('sudo rm /data/web_static/current')
         destination = "data/web_static/current"
-        run(f'ln -s /data/web_static/releases/{file_name} {destination}')
+        run(f'sudo ln -s /data/web_static/releases/{file_name} {destination}')
 
         return True
     except Exception as e:
