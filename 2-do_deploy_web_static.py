7e87242b9cea5c03b5f4353b134c2616e6aa7d2f
flowsar <brahim.sar151@gmail.com>
2024-07-13 15:29:49 +0100

    old version

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
 """pack web static"""
 
-from fabric import Connection
+from fabric.api import env, run, put, cd
+from fabric.decorators import task
+from fabric.context_managers import settings
 from datetime import datetime
+from os.path import exists
 
 env.hosts = ['35.175.64.12', '18.234.192.156']
-env.user = 'ubuntu' 
+env.user = 'ubuntu'
 env.key_filename = ['~/.ssh/id_rsa']
 
-
+@task
 def do_deploy(archive_path):
-    """generates a .tgz archive from the contents of the web_static"""
-    with Connection(env.hosts[0]) as server_conn1, Connection(env.hosts[1]) as server_conn2:
-        deply_by_server(server_conn1, archive_path)
-        deply_by_server(server_conn2, archive_path)
-
-
-def deply_by_server(server_conn, archive_path):
-    server_conn.put(archive_path, '/tmp/')
-    file_name = archive_path.split('/')[-1].split('.')[0]
-    result = None
-    with server_conn.run('cd /tmp/'):
-        command = f"tar -xvzf {file_name}.tgz -C /data/web_static/releases/{file_name}"
-        result = server_conn.run(command)
-        result = server_conn.run(f'rm -r {file_name}.tgz')
-    
-    with server_conn.run('cd /data/web_static/'):
-        server_conn.run('rm current')
-        result = command_sys_link = f'ln -s /data/web_static/releases/{file_name} /data/web_static/current'
-        result = server_conn.run(command_sys_link)
-    
-    if result.failed:
+    """Generates a .tgz archive from the contents of the web_static"""
+    if not exists(archive_path):
         return False
+    
+    file_name = archive_path.split('/')[-1].split('.')[0]
+
+    with settings(host_string=env.hosts[0]):
+        deploy_by_server(archive_path, file_name)
+
+    with settings(host_string=env.hosts[1]):
+        deploy_by_server(archive_path, file_name)
+
+def deploy_by_server(archive_path, file_name):
+    put(archive_path, '/tmp/')
+    with cd('/tmp/'):
+        run(f"tar -xvzf {file_name}.tgz -C /data/web_static/releases/{file_name}")
+        run(f'rm -r {file_name}.tgz')
+
+    with cd('/data/web_static/'):
+        run('rm current')
+        run(f'ln -s /data/web_static/releases/{file_name} /data/web_static/current')
+
     return True
