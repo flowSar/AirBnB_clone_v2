292a717d78c870513410966a82e8be9834e20d8a
flowsar <brahim.sar151@gmail.com>
2024-07-13 13:05:54 +0100

    fbric old version

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""pack web static"""
-from fabric import Connection
+""" pack web static"""
+from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """generates a .tgz archive from the contents of the web_static"""
-    local_conn = Connection('localhost')
-    local_conn.local('mkdir -p versions')
-    today = datetime.now().strftime("%Y%m%d%H%M%S")
-    arc_name = f"versions/web_static_{today}.tgz"
-    command = "tar -cvzf {} web_static/".format(arc_name)
-    result = local_conn.local(command)
+    """ generates a .tgz archive from the contents of the web_static """
+    local("mkdir -p versions")
+
+    date_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    arc_name = "versions/web_static_{}.tgz".format(date_time)
+
+    command = "tar -cvzf {} web_static".format(arc_name)
+    result = local(command)
     if result.failed:
         return None
     return arc_name
