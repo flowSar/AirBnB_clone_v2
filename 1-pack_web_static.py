896925d66c3e604de5e2732d72840c64d3210e97
flowsar <brahim.sar151@gmail.com>
2024-07-13 13:02:40 +0100

    up

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """pack web static"""
-
 from fabric import Connection
 from datetime import datetime
 
@@ -8,10 +7,10 @@ from datetime import datetime
 def do_pack():
     """generates a .tgz archive from the contents of the web_static"""
     local_conn = Connection('localhost')
-    local_conn.local('mkdir versions')
-    today = datetime.today().strftime('%Y%m%d%H%M%S')
+    local_conn.local('mkdir -p versions')
+    today = datetime.now().strftime("%Y%m%d%H%M%S")
     arc_name = f"versions/web_static_{today}.tgz"
-    command = f"tar -cvzf {arc_name} web_static/"
+    command = "tar -cvzf {} web_static/".format(arc_name)
     result = local_conn.local(command)
     if result.failed:
         return None
