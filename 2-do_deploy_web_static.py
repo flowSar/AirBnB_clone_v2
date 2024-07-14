39a7ca35e164a96d36dd682b97d7cdc5b8528439
flowsar <brahim.sar151@gmail.com>
2024-07-14 11:22:22 +0100

    fix unpressend distination and

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,19 +16,20 @@ def do_deploy(archive_path):
         return False
 
     file_name = archive_path.split('/')[-1].split('.')[0]
+
     try:
         put(archive_path, '/tmp/')
-        # cd('/tmp/')
         run(f'sudo mkdir -p /data/web_static/releases/')
         destination = "/data/web_static/releases/"
         run(f"sudo tar -xvzf /tmp/{file_name}.tgz -C {destination}")
         # we extract the file and move then from web_static to file_name
-        origin = f"/data/web_static/releases/{file_name}/web_static/*"
+        origin = f"/data/web_static/releases/web_static/*"
+        run(f"mkdir -p /data/web_static/releases/{file_name}")
         destination = f"/data/web_static/releases/{file_name}/"
         run(f"mv {origin} {destination}")
-
+        run("sudo rm -r /data/web_static/releases/web_static/")
         run(f'sudo rm -r /tmp/{file_name}.tgz')
-        # cd('/data/web_static/')
+
         run('sudo rm /data/web_static/current')
         destination = "/data/web_static/current"
         run(f'sudo ln -s /data/web_static/releases/{file_name} {destination}')
