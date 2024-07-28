6d7ab5b52ba5da17ad017a27bd08dd99d76a6f2b
flowsar <brahim.sar151@gmail.com>
2024-07-28 18:17:53 +0100

    another attempt

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- create two databases and and user=hbnb_dev
 -- grant all privileges to hbnb_dev_db and select for performance_schema
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE database IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,8 @@
 -- create two databases and and user=hbnb_test
 -- grant all privileges to hbnb_test_db and select for performance_schema
 
-CREATE database IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
-CREATE database IF NOT EXISTS performance_schema;
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+GRANT SELECT on performance_schema.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
