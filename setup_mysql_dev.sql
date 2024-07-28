d6c044c9f9c790d60d74b8d5f6d4df9dbfa6e502
flowsar <brahim.sar151@gmail.com>
2024-07-28 18:16:07 +0100

    another try

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,8 @@
 -- create two databases and and user=hbnb_dev
 -- grant all privileges to hbnb_dev_db and select for performance_schema
 
-CREATE database IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-CREATE DATABASE IF NOT EXISTS performance_schema;
 GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
