9db56ce64c709364300efdb54ab79f4a5c5745ba
flowsar <brahim.sar151@gmail.com>
2024-07-28 13:01:45 +0100

    render html file

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""web flask application extract subpath"""
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello2():
+    return 'HBNB'
+
+
+@app.route('/c/<path:subpath>', strict_slashes=False)
+def hello3(subpath):
+    text = subpath.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<path:subpath>', strict_slashes=False)
+def hello4(subpath=None):
+    if not subpath:
+        return 'Python is cool'
+    else:
+        text = subpath.replace('_', ' ')
+        return f'Python {text}'
+
+
+@app.route('/number/<int:subpath>', strict_slashes=False)
+def hello5(subpath=None):
+    return f'{subpath} is a number'
+
+
+@app.route('/number_template/<int:subpath>')
+def display(subpath):
+    return render_template('5-number.html', number=subpath)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
