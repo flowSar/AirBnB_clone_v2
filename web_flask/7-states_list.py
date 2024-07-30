a8e1624853b7d7ce1104196f7584e01fa722d48a
flowsar <brahim.sar151@gmail.com>
2024-07-30 08:52:03 +0100

    yp

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """fetch dada and display it"""
 from flask import Flask, request, render_template
-
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def display_data():
-    from models import storage
-    from models.state import State
     data = storage.all(State)
-    storage.close()
     return render_template('7-states_list.html', states=data)
+
 
 @app.teardown_appcontext
 def close_storage(exception=None):
@@ -22,4 +21,3 @@ def close_storage(exception=None):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
