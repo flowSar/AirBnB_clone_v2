db184038dc2a6b7d401952c26fa2cb40e9b27712
flowsar <brahim.sar151@gmail.com>
2024-07-30 09:14:29 +0100

    fix extracting data error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,8 +9,9 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def display_data():
-    data = storage.all(State)
-    return render_template('7-states_list.html', states=data)
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
