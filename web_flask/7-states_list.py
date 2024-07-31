5fb22247b85e3ca5838d8e8eb84d48b73180b51d
flowsar <brahim.sar151@gmail.com>
2024-07-31 22:31:54 +0100

    when if state if we're using mysql or json file to use right method to fetch data

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,7 +9,11 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def display_data():
-    states = storage.all(State)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        states = storage._DBStorage__session.query(State).all()
+    else:
+        states = storage.all(State)
     sorted_states = sorted(states.values(), key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
 
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
 """fetch dada and display it"""
 from flask import Flask, request, render_template
-
+from models import storage
+# from models.engine.db_storage import DBStorage
+from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def display_data():
-    from models import storage
-    from models.state import State
-    from models.city import City
-    states = storage.all(Sate)
-    state = State()
-    cities = state.cities()
+
+    cities = {}
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        states = storage._DBStorage__session.query(State).all()
+        for state in states:
+            cities[state.name] = []
+            print(f"State: {state.name}")
+            for city in state.cities:
+                cities[state.name] += [city.name]
+    else:
+        states = storage.all(State)
+        cities = State.cities()
     storage.close()
     return render_template('7-states_list.html', states=states, cities=cities)
+
 
 @app.teardown_appcontext
 def close_storage(exception=None):
     """tear down method"""
-    from models import storage
     storage.close()
 
 
